29fa11e014e11bfd2f4e890906c6834fad47d127
Jonathan Delgado <hi@jon.soy>
2023-07-13 15:02:17 -0400

    feat: add flag `--template`

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Date format to console.
 
 ```shell
 Usage: ndate [-] [--zero] [--date-style <full|long|medium|short>] [--time-style <full|long|medium|short>]
-             [--time-zone] [--local <locale>] [--json] [--epoch] [--epoch-ms] [--date <date>]
-             [--help] [-j] [-d <date>] [-l <locale>] [-z] [-h]
+             [--time-zone <time-zone>] [--local <locale>] [--template <template>]
+             [--json] [--epoch] [--epoch-ms] [--date <date>] [--help] [-j] [-d <date>]
+             [-l <locale>] [-z] [-h]
 ```
 
 Please install with deno:
@@ -27,4 +28,46 @@ echo 2022-10-12T05:39:19.678Z | ndate - # Wednesday, October 12, 2022 at 2:39:19
 ndate -j # 2022-10-12T05:40:33.246Z
 ndate --epoch # 1689255846
 ndate --epoch-ms # 1689255823663
+ndate --epoch-ms -z # 1689255823663%
+ndate --template '{{utc_full_year}}-{{utc_month:padStart:2:0}}-{{utc_date:padStart:2:0}}' # 2023-06-13
+```
+
+## Options to `--template`
+
+- `{{time}}`: Result of `date.getTime()`
+- `{{full_year}}`: Result of `date.getFullYear()`
+- `{{utc_full_year}}`: Result of `date.getUTCFullYear()`
+- `{{month}}`: Result of `date.getMonth()`
+- `{{utc_month}}`: Result of `date.getUTCMonth()`
+- `{{date}}`: Result of `date.getDate()`
+- `{{utc_date}}`: Result of `date.getUTCDate()`
+- `{{day}}`: Result of `date.getDay()`
+- `{{utc_day}}`: Result of `date.getUTCDay()`
+- `{{hours}}`: Result of `date.getHours()`
+- `{{utc_hours}}`: Result of `date.getUTCHours()`
+- `{{minutes}}`: Result of `date.getMinutes()`
+- `{{utc_minutes}}`: Result of `date.getUTCMinutes()`
+- `{{seconds}}`: Result of `date.getSeconds()`
+- `{{utc_seconds}}`: Result of `date.getUTCSeconds()`
+- `{{milliseconds}}`: Result of `date.getMilliseconds()`
+- `{{utc_milliseconds}}`: Result of `date.getUTCMilliseconds()`
+- `{{timezone_offset}}`: Result of `date.getTimezoneOffset()`
+- `{{local}}`: Result of `Intl.DateTimeFormat.format`
+- `{{local_day}}`: `day` part of `Intl.DateTimeFormat`
+- `{{local_dayPeriod}}`: `dayPeriod` part of `Intl.DateTimeFormat`
+- `{{local_era}}`: `era` part of `Intl.DateTimeFormat`
+- `{{local_hour}}`: `hour` part of `Intl.DateTimeFormat`
+- `{{local_literal}}`: `literal` part of `Intl.DateTimeFormat`
+- `{{local_minute}}`: `minute` part of `Intl.DateTimeFormat`
+- `{{local_month}}`: `month` part of `Intl.DateTimeFormat`
+- `{{local_second}}`: `second` part of `Intl.DateTimeFormat`
+- `{{local_timeZoneName}}`: `timeZoneName` part of `Intl.DateTimeFormat`
+- `{{local_weekday}}`: `weekday` part of `Intl.DateTimeFormat`
+- `{{local_year}}`: `year` part of `Intl.DateTimeFormat`
+
+
+**Sample**
+
+```shell
+ndate --template '{{utc_full_year}}-{{utc_month}}-{{utc_date}}' # 2023-6-13
 ```
--- ndate.ts
+++ ndate.ts
@@ -1,3 +1,69 @@
+
+function renderDateTemplate(template: string, date: Date, locales?: string, options?: Intl.DateTimeFormatOptions) {
+    const localeString = date.toLocaleString(locales, options)
+    const parts = new Intl.DateTimeFormat(locales, options).formatToParts(date).map(({ type, value }) => [`local_${type}`, value] as const)
+
+    const values = Object.fromEntries([
+        [`local`,localeString],
+        ["time", date.getTime()],
+        ["full_year", date.getFullYear()],
+        ["utc_full_year", date.getUTCFullYear()],
+        ["month", date.getMonth()],
+        ["utc_month", date.getUTCMonth()],
+        ["date", date.getDate()],
+        ["utc_date", date.getUTCDate()],
+        ["day", date.getDay()],
+        ["utc_day", date.getUTCDay()],
+        ["hours", date.getHours()],
+        ["utc_hours", date.getUTCHours()],
+        ["minutes", date.getMinutes()],
+        ["utc_minutes", date.getUTCMinutes()],
+        ["seconds", date.getSeconds()],
+        ["utc_seconds", date.getUTCSeconds()],
+        ["milliseconds", date.getMilliseconds()],
+        ["utc_milliseconds", date.getUTCMilliseconds()],
+        ["timezone_offset", date.getTimezoneOffset()],
+        ...parts,
+    ])
+
+    const transforms:Record<string, (value: string, options?: string)=>string> = {
+        json: value => JSON.stringify(value),
+        sub: (value, options) => {
+            const [start = 0,end] = options?.split(':') ?? []
+            return value.toString().substring(Number(start), Number(end))
+        },
+        padStart: (values,options) => {
+            const [maxLength = 0, fillString = ' '] = options?.split(':') ?? []
+            return values.toString().padStart(Number(maxLength), fillString)
+        },
+        padEnd: (values,options) => {
+            const [maxLength = 0, fillString = ' '] = options?.split(':') ?? []
+            return values.toString().padEnd(Number(maxLength), fillString)
+        },
+        default: value => value,
+    }
+
+    const regexp = /\{\{(?<keyword>\w+)(\:(?<transform>\w+)(\:(?<transform_options>[\w\:\-]+)))?\}\}/g
+    const stringTemplate:  string[] = []
+    const substitution:  string[] = []
+    let p: RegExpExecArray | null=null
+    let lastPointer = 0
+    do {
+        p = regexp.exec(template)
+        if (p) {
+            stringTemplate.push(template.substring(lastPointer, p.index))
+            lastPointer = p.index + p.at(0)!.length
+            const transform = transforms[p.groups?.transform ?? 'default'] ?? transforms.default
+            substitution.push(transform(values[p.groups!.keyword] ?? '', p.groups!.transform_options))
+        }
+    } while (p !== null);
+
+    stringTemplate.push(template.substring(lastPointer))
+
+    return String.raw({ raw: stringTemplate }, ...substitution)
+}
+
+
 const dateStyles = ["full", "long", "medium", "short"] as const;
 const timeStyles = ["full", "long", "medium", "short"] as const;
 type DateStyle = (typeof dateStyles)[number];
@@ -23,6 +89,7 @@ let outputAsEpochMS = false;
 let outputAsJSON = false;
 let stdinReadable = false;
 let showHelp = false;
+let template: string | undefined;
 let crontab: string | undefined;
 
 const parseCrontab = (str: string) => {
@@ -43,6 +110,7 @@ const transformOptions: Record<string, (nextArgument: () => string | undefined) 
     '--time-style': (nextArgument) => { timeStyle = toTimeStyle(nextArgument()) ?? 'full' },
     '--time-zone': (nextArgument)=>{ timeZone = nextArgument() },
     '--local': (nextArgument) => { local = nextArgument() },
+    '--template': (nextArgument) => { template = nextArgument() },
     '--json': () => { outputAsJSON = true },
     '--epoch': () => { outputAsEpoch = true },
     '--epoch-ms': () => { outputAsEpochMS = true },
@@ -65,6 +133,8 @@ const optionsLabels: Record<string, undefined | { label?: string }> = {
     '--time-style': { label: `<${timeStyles.join('|')}>` },
     '--local': { label: `<locale>` },
     '--date': { label: `<date>` },
+    '--template': { label: `<template>` },
+    '--time-zone': { label: `<time-zone>` },
     get "-j"() { return this['--json'] },
     get "-d"() { return this["--date"] },
     get "-l"() { return this["--local"] },
@@ -123,6 +193,7 @@ const run = async () => {
     }
 
     const toOutput = () => {
+        if (template) return renderDateTemplate(template, date, local, { dateStyle, timeStyle, timeZone })
         if (outputAsEpochMS) return Math.floor(date.getTime()).toString();
         if (outputAsEpoch) return Math.floor(date.getTime() / 1000).toString();
         if (outputAsJSON) return date.toJSON()
