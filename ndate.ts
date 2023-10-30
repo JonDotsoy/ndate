ad7538179419017c0d2954546fbfdd986027e492
Jonathan Delgado <hi@jon.soy>
2023-10-29 22:57:28 -0300

    feat: unwrap handler

--- ndate.ts
+++ ndate.ts
@@ -2,10 +2,6 @@
 
 import { handler } from "./handler.ts";
 
-const setup = async () => {
-    for await (const buff of handler(Deno.args)) {
-        Deno.stdout.write(buff)
-    }
+for await (const buff of handler(Deno.args)) {
+    Deno.stdout.write(buff)
 }
-
-await setup();
